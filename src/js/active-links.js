9be1a1a61b09f72fac0a427d7d0adb317ca7bde7
AngelinaSun <87764242+AngelinaSunny@users.noreply.github.com>
2022-05-25 15:10:32 +0300

    Add files via upload

---
+++ src/js/active-links.js
@@ -0,0 +1,31 @@
+//////////Навигация на основной странице////////
+
+const linksNav = document.querySelectorAll('.nav__list-item');
+const linkNavActive = 'nav__list-item--active';
+
+linksNav[0].classList.add(linkNavActive);
+
+linksNav.forEach(el => {
+  el.addEventListener('focus', () => {
+    linksNav.forEach(el => {
+      el.classList.remove(linkNavActive);
+    });
+    el.classList.add(linkNavActive);
+  })
+});
+
+//////////Навигация левого меню///////////////
+
+const leftMenuLinks = document.querySelectorAll('.left-nav-list__link');
+const linkLeftMenuActive = 'left-nav-list__link--active';
+
+leftMenuLinks[1].classList.add(linkLeftMenuActive);
+
+leftMenuLinks.forEach(link => {
+  link.addEventListener('focus', () => {
+    leftMenuLinks.forEach(link => {
+      link.classList.remove(linkLeftMenuActive);
+    });
+    link.classList.add(linkLeftMenuActive);
+  })
+});
