bacd6bfa2c41050b7c54ac945c7c62be9c5071ee
AngelinaSun <87764242+AngelinaSunny@users.noreply.github.com>
2022-05-25 17:20:55 +0300

    Add files via upload

--- src/js/active-links.js
+++ src/js/active-links.js
@@ -1,19 +1,3 @@
-//////////Навигация на основной странице////////
-
-const linksNav = document.querySelectorAll('.nav__list-item');
-const linkNavActive = 'nav__list-item--active';
-
-linksNav[0].classList.add(linkNavActive);
-
-linksNav.forEach(el => {
-  el.addEventListener('focusin', () => {
-    linksNav.forEach(el => {
-      el.classList.remove(linkNavActive);
-    });
-    el.classList.add(linkNavActive);
-  })
-});
-
 //////////Навигация левого меню///////////////
 
 const leftMenuLinks = document.querySelectorAll('.left-nav-list__link');
@@ -29,3 +13,18 @@ leftMenuLinks.forEach(link => {
     link.classList.add(linkLeftMenuActive);
   })
 });
+
+//////////Навигация на основной странице////////
+
+const linksNavItem = document.querySelectorAll('.nav__list-item');
+const linkNavActive = 'nav__list-item--active';
+
+
+linksNavItem.forEach(link => {
+  link.addEventListener('focusin', () => {
+    linksNavItem.forEach(link => {
+      link.classList.remove(linkNavActive);
+    });
+    link.classList.add(linkNavActive);
+  })
+})
--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ import '../../node_modules/focus-visible/dist/focus-visible';
 import '../scss/main.scss';
 import '../index.html';
 
-import Swiper, { Pagination } from 'swiper';
 import '../js/sliders';
 
 import '../js/buttons';
